37d59fd7c6be4652b99918660bdb8074cc4dee96
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-04-21 16:58:19 -0700

    Ready for check

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,13 +19,13 @@ if (ld > 5)
 {
 printf("Last digit of %i is %i and is greater than 5\n", n, ld);
 }
-else if (ld < 5)
+else if (ld == 0)
 {
-printf("Last digit of %i is %i and is less than 6 and not 0\n", n, ld);
+printf("Last digit of %i is %i and is 0\n", n, ld);
 }
 else
 {
-printf("Last digit of %i is %i and is 0\n", n, ld);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, ld);
 }
 return (0);
 }
