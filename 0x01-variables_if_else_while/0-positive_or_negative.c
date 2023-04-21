9569af0b159a0ad8bfc10aa4e12527abd003cb9f
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-04-21 16:25:40 -0700

    Ready for check

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%n is positive\n");
+printf("%i is positive\n");
 }
 else if (n == 0)
 {
-printf("%n is zero\n");
+printf("%i is zero\n");
 }
 else
 {
-printf("%n is negative\n");
+printf("%i is negative\n");
 }
 return (0);
 }
