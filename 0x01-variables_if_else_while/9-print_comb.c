3901eed96081c1246fc68f5e28a98ed2ff87a820
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-04-21 18:08:47 -0700

    Ready for check

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x = 48;
+
+while (x < 58)
+{
+	putchar (x);
+	if (x == 57)
+		break;
+	putchar (',');
+	putchar (' ');
+	x++;
+}
+putchar ('\n');
+return (0);
+}
