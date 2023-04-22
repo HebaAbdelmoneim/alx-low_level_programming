a7dad4d75aaf6f932f37bad86466d497e452456f
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-04-22 08:44:25 -0700

    Ready for check

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,17 +10,18 @@ int main(void)
 {
 int x, y;
 
-x = 45;
+x = 00;
 while (x < 99)
 {
 	y = x + 1;
 	while (y < 100)
 	{
-		if (sec
-		putchar (x);
+		putchar ((x / 10) + 48);
+		putchar ((x % 10) + 48);
 		putchar (' ');
-		putchar (y);
-		if (x == 98)
+		putchar ((y / 10) + 48);
+		putchar ((y % 10) + 48);
+		if (x == 98 && y == 99)
 			break;
 		putchar (',');
 		putchar (' ');
