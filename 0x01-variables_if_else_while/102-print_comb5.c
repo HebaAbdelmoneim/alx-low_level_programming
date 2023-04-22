8c0936ebfc17cc5e52c05619e82e6f6884c3cc0a
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-04-21 19:30:55 -0700

    Ready for check

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x, y;
+
+x = 45;
+while (x < 99)
+{
+	y = x + 1;
+	while (y < 100)
+	{
+		if (sec
+		putchar (x);
+		putchar (' ');
+		putchar (y);
+		if (x == 98)
+			break;
+		putchar (',');
+		putchar (' ');
+		y++;
+	}
+	x++;
+}
+putchar ('\n');
+return (0);
+}
