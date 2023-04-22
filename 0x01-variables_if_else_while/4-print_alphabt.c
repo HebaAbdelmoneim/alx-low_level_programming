e8871897e9531085245849877a66e52cea514f50
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-04-21 17:31:55 -0700

    Ready for check

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x = 97;
+
+while (x < 123)
+{
+	if (x != 113 && x != 101)
+	putchar (x);
+	x++;
+}
+putchar ('\n');
+return (0);
+}
