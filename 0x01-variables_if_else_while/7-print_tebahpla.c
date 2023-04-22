71369de248c9f280c0a28db79a5678de6936c18f
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-04-21 17:55:16 -0700

    Ready for check

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x = 122;
+
+while (x > 96)
+{
+	putchar (x);
+	x--;
+}
+putchar ('\n');
+return (0);
+}
