ef1bcb8b94c17f3e054f35d2f7c7b0fb5735c57b
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-04-26 09:41:08 -0700

    Ready for check

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,7 +13,7 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10) /* The while loop need to break before entering the infinite loop */ 
+	while (i < 10)/* The while loop need to increment i */
 	{
 		putchar(i);
 	}
