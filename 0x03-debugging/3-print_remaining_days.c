d724a8d91b1d2db972280b0b7e5c530b4a707e8b
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-04-26 12:26:39 -0700

    Ready for check

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,6 +12,12 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+	/*
+	 * leap year when it can be divided by 4 or
+	 * divided by 100 and 400
+	 *
+	 */
+
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
