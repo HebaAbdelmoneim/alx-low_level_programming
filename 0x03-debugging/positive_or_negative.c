41b2184fcbc7e780f9e4b2fc332a6366b3144392
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-04-26 08:55:14 -0700

    Ready for check

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * positive_or_negative -check if number is +ve or  -ve
+ *
+ * @i: input pram
+ *
+ * Return: Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%i is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%i is zero\n", i);
+	}
+	else
+	{
+		printf("%i is negative\n", i);
+	}
+}
