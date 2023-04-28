75f7f5357110d1b05939661c0a2c6d6e1a6d804f
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-04-27 18:51:29 -0700

    Ready for check

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,58 +1,6 @@
 #include <stdio.h>
 
 /**
- * _sqr - calculate the square mean root of the input
- *
- *@a: input parameter
- *
- * Return: the sqr value
- */
-
-double _sqr(double a)
-{
-	double i = 0;
-	double j = a / 2;
-
-	while (j != i)
-	{
-		i = j;
-		j = (a / i + i) / 2;
-	}
-	return (j);
-}
-/**
- * largest_prime - print the largest prime factor
- *
- *@n: input parameter
- *
- * Return: void
- */
-
-void largest_prime(long int n)
-{
-	int l_p, p_n;
-
-	while (n % 2 == 0)
-	{
-		n = n / 2;
-	}
-
-	for (p_n = 3; p_n <= _sqr(n); p_n += 2)
-	{
-		while (n % p_n == 0)
-		{
-			n = n / p_n;
-			l_p = p_n;
-		}
-	}
-
-	if (n > 2)
-		l_p = n;
-
-	printf("%d\n", l_p);
-}
-
-/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -60,6 +8,7 @@ void largest_prime(long int n)
 
 int main(void)
 {
-	largest_prime(612852475143);
+	int x = 50829599;
+	printf("%d\n", x); 
 	return (0);
 }
