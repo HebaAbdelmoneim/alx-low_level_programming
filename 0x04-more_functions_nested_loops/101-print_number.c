2f96168f423ca58ce7fbc7d85e6a08f2da30bc2f
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-04-27 18:14:45 -0700

    Ready for check

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,7 @@
 
 void print_number(int n)
 {
-	int i;
+	unsigned int i;
 
 	if (n >= 0)
 	{
@@ -23,8 +23,7 @@ void print_number(int n)
 	}
 
 	if ((i / 10) > 0)
-		print_number (i / 10);
-
+		print_number(i / 10);
 
 	_putchar((i % 10) + 48);
 }
