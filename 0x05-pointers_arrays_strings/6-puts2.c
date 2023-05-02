2d486f71c30eee37bd67792c0d1db37b226ca701
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-02 13:54:53 -0700

    Ready for check

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,14 @@
 
 void puts2(char *str)
 {
+	int i = 0;
+
 	while (*str != '\0')
 	{
-		_putchar(*str);
-		str += 2;
+		if (i % 2 == 0)
+			_putchar(*str);
+		str++;
+		i++;
 	}
 	_putchar('\n');
 }
