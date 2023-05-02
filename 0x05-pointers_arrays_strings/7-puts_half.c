38598d7e07d7e24dbe846877a6c1dc6250f93572
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-02 14:20:01 -0700

    Ready for check

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - print the second half of the string
+ *@str: pointer to the char value of s
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int length, i = 0;
+
+	length = strlen(str);
+	while (*str != '\0')
+	{
+		if (i >= (length / 2))
+		{
+			_putchar(*str);
+		}
+		str++;
+		i++;
+	}
+	_putchar('\n');
+}
