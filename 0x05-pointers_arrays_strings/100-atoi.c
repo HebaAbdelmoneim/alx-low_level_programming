d7948b9666f7b5b70cba47315527861d4f770265
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-02 16:19:38 -0700

    Ready for check

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _atoi - convert the numbers in string to integer
+ *@s: pointer to string
+ *
+ * Return: integer of the input char
+ */
+
+int _atoi(char *s)
+{
+	int length, i, digit, oint = 0, sign = 0;
+	char prev;
+
+	length = strlen(s);
+	for (i = 0; i <= length; i++)
+	{
+		if (*s == 45)
+			sign++;
+		if (*s >= 48 && *s <= 57)
+		{
+			digit = *s - 48;
+			oint = (oint * 10) + digit;
+		}
+		prev = *s;
+		s++;
+		if (!(*s >= 48 && *s <= 57) && (prev >= 48 && prev <= 57))
+			break;
+	}
+	if (sign % 2 != 0)
+		oint *= -1;
+	return (oint);
+}
+
