5f1d42c81c9a5c232979c0539f8a2a8510e92eb2
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-02 13:38:17 -0700

    Ready for check

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print even or odd depending on the first number in the string
+ *
+ *@str: pointer to the char value of s
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
