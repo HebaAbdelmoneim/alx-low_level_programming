c7a8d5ef8a2d9ae3e84e9e56c0c48355e7526931
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-02 18:37:23 -0700

    Ready for check

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,42 +9,17 @@
  */
 int main(void)
 {
-    int n, i;
-    char l[] = "abcdefghijklmnoqprstuvwyzx";
-    char L[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-    char no[] = "0123456789";
-    char ch[] = "!@#$%^&*+-?&";
-    char pw [15];
+	int  check = 0;
+	char pw;
 
-    srand(time(0));
-    n = rand() % 4;
-   for (i = 0; i < 10; i++)
-   {
-       if (n == 1)
-        {
-            pw[i] = no[rand() % 10];
-            n = rand() % 4;
-            printf("%c", pw[i]);
-        }
-        else if (n == 2)
-        {
-            pw[i] = l[rand() % 26];
-            n = rand() % 4;
-            printf("%c", pw[i]);
-        }
-        else if (n == 3)
-        {
-            pw[i] = L[rand() % 26];
-            n = rand() % 4;
-            printf("%c", pw[i]);
-        }
-        else
-        {
-            pw[i] = ch[rand() % 12];
-            n = rand() % 4;
-            printf("%c", pw[i]);
-        }
-   }
-    return (0);
+	srand(time(NULL));
+
+	while (check <= 2645)
+	{
+		pw = rand() % 128;
+		putchar(pw);
+		check += pw;
+	}
+	putchar(2772 - check);
+	return (0);
 }
-
