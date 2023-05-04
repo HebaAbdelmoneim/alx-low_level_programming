edcbe3d36b8cc0f3c9769a55689df3e6045e489a
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-04 08:04:52 -0700

    Ready for check

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - compares two string length
+ *@s1: pointer to the first string array
+ *@s2: pointer to the second string array
+ *
+ * Return: 0 if the two strings have the same length.
+ *1 if the first string is longer
+ *-1 if the second string is longer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int s1_len, s2_len, res;
+
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+
+	if (s1_len - s2_len > 0)
+		res = 1;
+	else if (s1_len - s2_len < 0)
+		res = -1;
+	else
+		res = 0;
+	return (res);
+}
