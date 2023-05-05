870a349de260846ba5649e12767c46db80d561eb
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-04 18:19:18 -0700

    Ready for check

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,13 +11,16 @@
 
 void print_number(int n)
 {
+	unsigned int x;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		x = -1 * n;
 	}
-	if ((n / 10) > 0)
-		print_number(n / 10);
-	_putchar((n % 10) + 48);
+	else
+		x = n;
+	if ((x / 10) > 0)
+		print_number(x / 10);
+	_putchar((x % 10) + 48);
 }
