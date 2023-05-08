5e4008f1314edba0f478c52ddf74393f4f601d5a
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-08 14:34:57 -0700

    Ready for check

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ *@haystack: pointer to char array
+ *@needle: pointer to another char array
+ *
+ * Return: pointer to char array
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int k;
+
+	k = 0;
+	while (haystack)
+	{
+		k = 0;
+		while (haystack[k] == needle[k])
+		{
+			if (haystack[k] == needle[k])
+			{
+				if (needle[k] == '\0')
+					return (haystack);
+			}
+			k++;
+		}
+		haystack++;
+	}
+	return ('\0');
+}
