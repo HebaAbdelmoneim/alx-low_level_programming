ac269a066c285ecc68e0fffbc57067ae19cea052
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-08 10:43:53 -0700

    Ready for check

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ *@s: pointer to char array
+ *@c: char input
+ *
+ * Return: pointer to array of char
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	int j = 0;
+
+	while (*(s + j) != '\0')
+	{
+		if (*(s + j) == c)
+			return (s + j);
+		j++;
+	}
+	return (0);
+}
