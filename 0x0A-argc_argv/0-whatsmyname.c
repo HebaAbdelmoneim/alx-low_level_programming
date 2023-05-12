05f18028ce99e0a3c657d5607500b6471047f2ae
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-12 15:10:59 -0700

    Ready for check

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *@argc: arg number
+ *@argv: arg string array
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
