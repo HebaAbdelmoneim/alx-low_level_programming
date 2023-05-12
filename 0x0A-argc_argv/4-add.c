25b8d7963a4c9557e7cb33ce48168915bc187fe4
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-12 16:51:42 -0700

    Ready for check

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ *@argc: arg numbers
+ *@argv: array of arg string
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int sum, i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) != 0)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
+}
