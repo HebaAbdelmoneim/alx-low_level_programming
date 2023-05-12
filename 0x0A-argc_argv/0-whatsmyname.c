ba0e155ffade4fef68dc820c942e9dcdaa91cee6
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-12 15:46:38 -0700

    Ready for check

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ *@argc: arg numbers
+ *@argv: array of arg string
+ *
+ * Return: Always 0.
+ */
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
