671bfb9399a838d4426a2a53583d0f5cd6789d47
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-12 15:30:48 -0700

    Ready for check

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the arg number
+ *@argc: arg numbers
+ *@argv: array of arg string
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char  __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
