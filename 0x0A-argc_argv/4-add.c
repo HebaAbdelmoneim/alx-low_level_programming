905c667a5cf03c37e6442c527b9c30111acdac6a
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-12 17:28:21 -0700

    Ready for check

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,20 +12,21 @@
 int main(int argc, char *argv[])
 {
 	int sum, i;
+	char *nm;
+
+	nm = argv[0];
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) > 0)
-			{
-				sum += atoi(argv[i]);
-			}
-			else
+			if (*(nm + i) < '0' || *(nm + i) > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
+			else
+				sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 		return (0);
