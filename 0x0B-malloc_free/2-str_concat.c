e81db6f16dbb0e52a0d144cbbfdd8dd42afa828f
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-16 08:46:04 -0700

    Ready for check

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	char *ar;
 	int i, l1, l2, len;
 
-	if (s1 == NULL)
+	if (*s1 == NULL)
 		*s1 = '';
-	if (s2 == NULL)
+	if (*s2 == NULL)
 		*s2 = '';
 	l1 = strlen(s1);
 	l2 = strlen(s2);
