5e4a3bb280f08b97cdffdca8211163769c3576a5
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-18 12:55:47 -0700

    Ready for check

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,6 +4,26 @@
 #include <string.h>
 
 /**
+ * set_mem - intialize the array
+ * @ar: array 
+ * @c: the itialization value
+ * @size: array size
+ *
+ * Return: pointer to the array
+ */
+char *set_mem(void *ar, char c, unsigned int size)
+{
+	char *ind = ar;
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		ind[i] = c;
+	}
+	return (ind);
+}
+
+/**
  * _calloc - allocates memory for an array
  * @nmemb: array members
  * @size: size of each member
@@ -13,18 +33,12 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ar;
-	char *ind;
-	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ar = malloc(nmemb * size);
 	if (ar == NULL)
 		return (NULL);
-	ind = ar;
-	for (i = 0; i < nmemb; i++)
-	{
-		ind[i] = 0;
-	}
+	set_mem(ar, 0, nmemb * size);
 	return (ar);
 }
