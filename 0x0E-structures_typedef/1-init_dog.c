07953f8cb400e1ca98a116894ab3dfa461d9042c
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-22 15:47:48 -0700

    Ready for check

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,9 +14,12 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	struct dog *new_dog = d;
-
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
+	
+	if (new_dog != 0)
+	{
+		new_dog->name = name;
+		new_dog->age = age;
+		new_dog->owner = owner;
+	}
 
 }
