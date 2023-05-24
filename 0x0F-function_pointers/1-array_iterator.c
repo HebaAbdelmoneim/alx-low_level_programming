5a8771a9421ec654bdae58e4d7f997e0ec879bed
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-24 08:06:06 -0700

    Ready for check

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter
+ * @array: pointer to integer
+ * @size: size of the array
+ * @action: ppinter to function
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (action != NULL)
+	{
+		for (i  = 0; i < (int)size; i++)
+			action(array[i]);
+	}
+}
