0cd570db13e996ab832a7e392212aeba473db2a1
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-25 16:17:31 -0700

    Ready for check

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,56 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - print char
+ * @arg: the arg to be printed
+ *
+ * Return: void
+ */
+void print_char(va_list arg)
+{
+	char c = va_arg(arg, int);
+	printf("%c", c);
+}
+
+/**
+ * print_int - print int
+ * @arg: the arg to be printed
+ *
+ * Return: void
+ */
+void print_int(va_list arg)
+{
+        int in = va_arg(arg, int);
+        printf("%i", in);
+}
+
+/**
+ * print_float - print float
+ * @arg: the arg to be printed
+ *
+ * Return: void
+ */
+void print_float(va_list arg)
+{
+        float fl = va_arg(arg, double);
+        printf("%f", fl);
+}
+
+/**
+ * print_str - print string
+ * @arg: the arg to be printed
+ *
+ * Return: void
+ */
+void print_str(va_list arg)
+{
+        char *st = va_arg(arg, char *);
+	if (st == NULL)
+		printf("(nil)");
+        printf("%s", st);
+}
+
+/**
  * print_all - print any type
  * @format: pointer to string
  *
@@ -10,35 +60,27 @@
 void print_all(const char * const format, ...)
 {
 	va_list pa;
-	unsigned int i = 0, n;
+	unsigned int i = 0, j = 0, n;
 	char ch;
-	char *st;
+	f_type tys[] = {
+        {"c", print_char},
+        {"i", print_int},
+        {"f", print_float},
+        {"s", print_str},
+        {NULL, NULL}
+        };
 
 	n = strlen(format);
 	va_start(pa, format);
-
 	while (i < n)
 	{
 		ch  = format[i];
-		switch (ch){
-		case 'c':
-			printf("%c",  va_arg(pa, int));
-			break;
-		case 'i':
-			printf("%i", va_arg(pa, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(pa, double));
-			break;
-		}
-		if (ch == 's')
+		j = 0;
+		while (tys[j].t)
 		{
-			st = va_arg(pa, char *);
-			if (st == NULL)
-			{
-				printf("(nil)");
-			}
-			printf("%s", st);
+			if (ch == tys[j].t[0])
+				tys[j].f(pa);
+			j++;
 		}
 		printf("%s", i < (n - 1) ? ", " : "\n");
 		i++;
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,9 +5,20 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * struct ty - Struct ty
+ *
+ * @t: The parameter type
+ * @f: The function associated
+ */
+typedef struct ty
+{
+        char *t;
+        void (*f)(va_list);
+} f_type;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 #endif /*VARIADIC_FUNCTIONS_H*/
