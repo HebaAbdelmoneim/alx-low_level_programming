de01d824a3a407f945de12105691ad3607795015
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-05-25 08:40:02 -0700

    Ready for cheeck

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum of all its parameters
+ * @n: parameters numbers
+ *
+ * Return: integer summation
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list pa;
+	unsigned int i;
+	int sum = 0;
+
+	/*if (n == 0)*/
+	/*	return (0);*/
+	va_start(pa, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(pa, int);
+	va_end(pa);
+	return (sum);
+}
