03677ea2198881b4b6c801817fdfcfb86299cdb4
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-06-01 15:50:40 -0700

    Ready for check

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,16 +11,22 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-        list_t *temp;
+	list_t *temp;
 	list_t *end_node;
 
 	end_node = *head;
-        temp = malloc(sizeof(list_t));
-        if (temp == NULL)
-                return (NULL);
-        temp->str = strdup(str);
-        temp->len = strlen(str);
-        temp->next = *head;
-        *head = temp;
-        return (end_node);
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->str = strdup(str);
+	temp->len = strlen(str);
+	if (end_node != NULL)
+	{
+		while (end_node->next != NULL)
+			end_node = end_node->next;
+		end_node->next = temp;
+	}
+	else
+		*head = temp;
+	return (temp);
 }
