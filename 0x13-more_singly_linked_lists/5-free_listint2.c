791029b275c8245d06db91aa65a38a68d9ed3812
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-06-05 09:34:04 -0700

    Ready for check

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,11 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *node = *head;
+	listint_t *temp, *node;
 
-	if (!node)
+	if (!head)
 		return;
+	node = *head;
 	while (node)
 	{
 		temp = node->next;
