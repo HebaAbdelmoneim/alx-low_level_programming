323349f3de94fbfc88353be349c5c2487eaab49d
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-06-06 06:53:39 -0700

    Ready for check

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,12 +14,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *node;
 	unsigned int i;
 
-	if (!head)
-		return (NULL);
 	node = head;
 	for (i = 0; i < index; i++)
 	{
 		node = node->next;
 	}
-	return (node->next);
+	return (node);
 }
