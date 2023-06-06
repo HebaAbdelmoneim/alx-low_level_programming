ee278c7513809273dea71924640b7c9065737935
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-06-06 16:50:04 -0700

    Ready for check

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	node = *head;
-	for (i = 0; i < index - 1; i++)
+	for (i = 0; i < index; i++)
 	{
 		temp = node;
 		node = node->next;
