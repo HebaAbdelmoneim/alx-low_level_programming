fb6a2c567bfa8ccde622ec15093c8cceb481234f
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-06-06 16:18:42 -0700

    Ready for check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,17 +14,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node, *new_node;
 	unsigned int i;
-	
+
 	new_node = malloc(sizeof(listint_t));
-	if (!new_node || !(*head) || !head)
+	if (!new_node || !head)
 		return (NULL);
 	node = *head;
+	if (!idx)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < idx - 1; i++)
 	{
 		node = node->next;
 		if (!node)
 			return (NULL);
-        }
+	}
 	new_node->next = node->next;
 	new_node->n = n;
 	node->next = new_node;
