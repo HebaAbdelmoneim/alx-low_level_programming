a811232e088c255f4ecfbae8636cd370997787a8
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-06-12 16:27:32 -0700

    Ready for check

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,12 +15,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-        f = open(filename, O_RDWR | O_CREAT | O_APPEND);
-        if (text_content)
-                len = strlen(text_content);
-        w = write(f, text_content, len);
-        if (f == -1 || w == -1)
-                return (-1);
-        close(f);
-        return (1);
+	f = open(filename, O_RDWR | O_CREAT | O_APPEND);
+	if (text_content)
+		len = strlen(text_content);
+	w = write(f, text_content, len);
+	if (f == -1 || w == -1)
+		return (-1);
+	close(f);
+	return (1);
 }
