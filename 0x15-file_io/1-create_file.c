908983efc19b5e1be7248b661da4b329b954b8c1
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-06-12 15:38:48 -0700

    Ready for check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,12 +11,14 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int f, w;
+	int f, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	w = write(f, text_content, strlen(text_content));
+	if (text_content)
+		len = strlen(text_content);
+	w = write(f, text_content, len);
 	if (f == -1 || w == -1)
 		return (-1);
 	close(f);
