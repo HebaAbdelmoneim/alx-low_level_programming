bc019a056a3146262e559dea1dd2c510907c8fe5
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-06-12 17:03:51 -0700

    Ready for check

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * @ac: var number
+ * @av: variables
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	int f1, f2, w, r;
+	char *buf;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s cp file_from file_to\n", av[0]);
+		exit(97);
+	}
+	if (av[1] == NULL)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	f1 = open(av[2], O_RDWR | O_CREAT | O_APPEND | O_TRUNC, 0664);
+	f2 = open(av[1], O_RDONLY);
+	buf = malloc(sizeof(char) * 1024);
+	r = read(f2, buf, 1024);
+	w = write(f1, buf, r);
+	if (f1 == -1 || w == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	free(buf);
+	close(f1);
+	close(f2);
+	return (0);
+}
