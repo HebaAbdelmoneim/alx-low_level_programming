6cb4edb79d575bbfa2b244b482785aac0270793e
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-06-12 17:19:25 -0700

    Ready for check

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,14 +23,20 @@ int main(int ac, char **av)
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	f1 = open(av[2], O_RDWR | O_CREAT | O_APPEND | O_TRUNC, 0664);
+	f1 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	f2 = open(av[1], O_RDONLY);
 	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 	r = read(f2, buf, 1024);
 	w = write(f1, buf, r);
 	if (f1 == -1 || w == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		free(buf);
 		exit(99);
 	}
 	free(buf);
