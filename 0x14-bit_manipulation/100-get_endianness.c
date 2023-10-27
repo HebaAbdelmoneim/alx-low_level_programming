aed751e8e5c982443b3f3d3050eeb86911b67e60
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-10-27 11:05:44 -0700

    Ready for check

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned long int x = 1;
+	int res;
+
+	res = (*(char *)&x);
+	return (res);
+}
+
