196d7a527cc810c5a432261afe522f0251f47201
HebaAbdelmoneim <heba_abd_elmoneim@yahoo.com>
2023-10-30 16:39:01 -0700

    Ready for check

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,11 +6,15 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_mag(Elf64_Ehdr hd);
+void print_cl(Elf64_Ehdr hd);
+void print_dt(Elf64_Ehdr hd);
+void print_vr(Elf64_Ehdr hd);
 
 #endif /* MAIN_H */
 
